b718864216fde5cb7c49bb19844799c66c3461d9
Chaiyi <chaiyi.fun@qq.com>
2025-09-06 14:10:05 +0800

    重构为基于GRPC，弃用原生RPC

---
+++ contract/notify/contract_gen.go
@@ -0,0 +1,27 @@
+package notify
+
+import (
+	"context"
+	"github.com/Gong-Yang/g-micor/core/discover"
+	"google.golang.org/grpc"
+	"log/slog"
+)
+
+var Client NotifyClient = &notifyRemoteClient{}
+
+type notifyRemoteClient struct {
+	client NotifyClient
+}
+
+func (n *notifyRemoteClient) SendEmail(ctx context.Context, in *SendEmailRequest, opts ...grpc.CallOption) (*SendEmailResponse, error) {
+	if n.client == nil {
+		c, err := discover.Grpc("notify")
+		if err != nil {
+			return nil, err
+		}
+		client := NewNotifyClient(c)
+		n.client = client
+		slog.Info("notify remote client init")
+	}
+	return n.client.SendEmail(ctx, in, opts...)
+}
