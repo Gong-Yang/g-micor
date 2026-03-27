01dadc80a978bb111926fa795597e021e584145f
gong.yang <chaiyi.fun@qq.com>
2026-03-27 18:01:27 +0800

    pgx封装自测

--- pgsql/table.go
+++ pgsql/table.go
@@ -1,4 +1,4 @@
-package main
+package pgsql
 
 import (
 	"context"
@@ -28,6 +28,7 @@ const (
 	scanPtrInt
 	scanPtrUint
 	scanPtrFloat
+	scanPtrTime
 )
 
 type fieldMeta struct {
@@ -47,88 +48,6 @@ type Table[T DBEntity] struct {
 	insertFields []*fieldMeta
 	insertOneSQL string
 	selectOneSQL string
-}
-
-// ---- 查询条件构建 ----
-
-type WhereBuilder struct {
-	conditions []string
-	args       []any
-	orderBy    string
-	limit      int
-	offset     int
-}
-
-func Where(condition string, args ...any) *WhereBuilder {
-	wb := &WhereBuilder{}
-	return wb.And(condition, args...)
-}
-
-func (wb *WhereBuilder) And(condition string, args ...any) *WhereBuilder {
-	wb.conditions = append(wb.conditions, condition)
-	wb.args = append(wb.args, args...)
-	return wb
-}
-
-func (wb *WhereBuilder) OrderBy(orderBy string) *WhereBuilder {
-	wb.orderBy = orderBy
-	return wb
-}
-
-func (wb *WhereBuilder) Limit(limit int) *WhereBuilder {
-	wb.limit = limit
-	return wb
-}
-
-func (wb *WhereBuilder) Offset(offset int) *WhereBuilder {
-	wb.offset = offset
-	return wb
-}
-
-// reindex 将条件中的 $1, $2... 占位符按照 startIdx 重新编号
-// 这样在 InsertMany 等场景拼 SQL 时不会冲突
-func (wb *WhereBuilder) buildSQL(startIdx int) (string, []any) {
-	if wb == nil || len(wb.conditions) == 0 {
-		return "", nil
-	}
-
-	// 重新编号占位符
-	var reindexed []string
-	argIdx := startIdx
-	for _, cond := range wb.conditions {
-		var newCond strings.Builder
-		i := 0
-		for i < len(cond) {
-			if cond[i] == '$' {
-				// 跳过原始的 $N
-				j := i + 1
-				for j < len(cond) && cond[j] >= '0' && cond[j] <= '9' {
-					j++
-				}
-				newCond.WriteString(fmt.Sprintf("$%d", argIdx))
-				argIdx++
-				i = j
-			} else {
-				newCond.WriteByte(cond[i])
-				i++
-			}
-		}
-		reindexed = append(reindexed, newCond.String())
-	}
-
-	clause := " WHERE " + strings.Join(reindexed, " AND ")
-
-	if wb.orderBy != "" {
-		clause += " ORDER BY " + wb.orderBy
-	}
-	if wb.limit > 0 {
-		clause += fmt.Sprintf(" LIMIT %d", wb.limit)
-	}
-	if wb.offset > 0 {
-		clause += fmt.Sprintf(" OFFSET %d", wb.offset)
-	}
-
-	return clause, wb.args
 }
 
 // ---- 分页结果 ----
@@ -329,6 +248,15 @@ func (t *Table[T]) prepareScan(val reflect.Value) *scanContext {
 				target:     &nf,
 			})
 
+		case scanPtrTime:
+			var nt sql.NullTime
+			sc.scanArgs[i] = &nt
+			sc.ptrSlots = append(sc.ptrSlots, ptrScanSlot{
+				fieldIndex: field.Index,
+				kind:       scanPtrTime,
+				elemType:   field.ElemType,
+				target:     &nt,
+			})
 		default:
 			sc.scanArgs[i] = fieldVal.Addr().Interface()
 		}
@@ -691,6 +619,20 @@ func (s ptrScanSlot) apply(val reflect.Value) error {
 		p := reflect.New(s.elemType)
 		p.Elem().SetFloat(nf.Float64)
 		fieldVal.Set(p)
+
+	case scanPtrTime:
+		nt := s.target.(*sql.NullTime)
+		if !nt.Valid {
+			fieldVal.Set(reflect.Zero(fieldVal.Type()))
+			return nil
+		}
+		if s.elemType != nil {
+			p := reflect.New(s.elemType)
+			p.Elem().Set(reflect.ValueOf(nt.Time))
+			fieldVal.Set(p)
+		} else {
+			fieldVal.Set(reflect.ValueOf(nt.Time))
+		}
 	}
 
 	return nil
@@ -704,7 +646,7 @@ func classifyScanKind(fieldType reflect.Type) (fieldScanKind, reflect.Type) {
 		elem := fieldType.Elem()
 		if elem.Kind() == reflect.Struct {
 			if elem == timeType { // 时间类型
-				return scanDirect, nil
+				return scanPtrTime, elem
 			}
 			// 先查 *T（指针接收者），再查 T（值接收者）
 			if fieldType.Implements(scannerType) || fieldType.Implements(valuerType) {
@@ -741,7 +683,7 @@ func classifyScanKind(fieldType reflect.Type) (fieldScanKind, reflect.Type) {
 	// 非指针 struct
 	if fieldType.Kind() == reflect.Struct {
 		if fieldType == timeType {
-			return scanDirect, nil
+			return scanPtrTime, nil
 		}
 		// 值接收者
 		if fieldType.Implements(scannerType) || fieldType.Implements(valuerType) {
---
+++ pgsql/where.go
@@ -0,0 +1,88 @@
+package pgsql
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ---- 查询条件构建 ----
+
+type WhereBuilder struct {
+	conditions []string
+	args       []any
+	orderBy    string
+	limit      int
+	offset     int
+}
+
+func Where(condition string, args ...any) *WhereBuilder {
+	wb := &WhereBuilder{}
+	return wb.And(condition, args...)
+}
+
+func (wb *WhereBuilder) And(condition string, args ...any) *WhereBuilder {
+	wb.conditions = append(wb.conditions, condition)
+	wb.args = append(wb.args, args...)
+	return wb
+}
+
+func (wb *WhereBuilder) OrderBy(orderBy string) *WhereBuilder {
+	wb.orderBy = orderBy
+	return wb
+}
+
+func (wb *WhereBuilder) Limit(limit int) *WhereBuilder {
+	wb.limit = limit
+	return wb
+}
+
+func (wb *WhereBuilder) Offset(offset int) *WhereBuilder {
+	wb.offset = offset
+	return wb
+}
+
+// reindex 将条件中的 $1, $2... 占位符按照 startIdx 重新编号
+// 这样在 InsertMany 等场景拼 SQL 时不会冲突
+func (wb *WhereBuilder) buildSQL(startIdx int) (string, []any) {
+	if wb == nil || len(wb.conditions) == 0 {
+		return "", nil
+	}
+
+	// 重新编号占位符
+	var reindexed []string
+	argIdx := startIdx
+	for _, cond := range wb.conditions {
+		var newCond strings.Builder
+		i := 0
+		for i < len(cond) {
+			if cond[i] == '$' {
+				// 跳过原始的 $N
+				j := i + 1
+				for j < len(cond) && cond[j] >= '0' && cond[j] <= '9' {
+					j++
+				}
+				newCond.WriteString(fmt.Sprintf("$%d", argIdx))
+				argIdx++
+				i = j
+			} else {
+				newCond.WriteByte(cond[i])
+				i++
+			}
+		}
+		reindexed = append(reindexed, newCond.String())
+	}
+
+	clause := " WHERE " + strings.Join(reindexed, " AND ")
+
+	if wb.orderBy != "" {
+		clause += " ORDER BY " + wb.orderBy
+	}
+	if wb.limit > 0 {
+		clause += fmt.Sprintf(" LIMIT %d", wb.limit)
+	}
+	if wb.offset > 0 {
+		clause += fmt.Sprintf(" OFFSET %d", wb.offset)
+	}
+
+	return clause, wb.args
+}
